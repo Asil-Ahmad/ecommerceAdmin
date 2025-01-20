7a7ba7cce88b3e9644efbc00d316be221635d706
Asil Ahmad <identity101@duck.com>
2025-01-20 10:22:27 +0530

    Working on about page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/product/:_id' element={<ProductPage />} />
+        <Route path='*' element={<Home />} /> // todo Add 404 page
       </Routes>
     </div>
   );
