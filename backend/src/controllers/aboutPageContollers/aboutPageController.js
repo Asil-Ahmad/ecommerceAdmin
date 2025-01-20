910f77f47b17b298b5df8f82a3a118244a862d11
Asil Ahmad <identity101@duck.com>
2025-01-20 17:31:44 +0530

    🔧 fix: Added update functionality for About Page but issues persist

--- backend/src/controllers/aboutPageContollers/aboutPageController.js
+++ backend/src/controllers/aboutPageContollers/aboutPageController.js
@@ -3,8 +3,6 @@ import { v2 as cloudinary } from "cloudinary";
 import upload from "../../middleware/multer.js";
 import aboutPageModel from "../../models/aboutPageUI/aboutPageModel.js";
 
-
-
 export const addAboutPage = async (req, res) => {
   const { link, text, para, buttonText, bgColor } = req.body;
 
@@ -41,5 +39,37 @@ export const getAboutPage = async (req, res) => {
     res
       .status(400)
       .json({ success: false, message: "Failed to fetch about page", error: error.message });
+  }
+};
+
+export const updateAboutPage = async (req, res) => {
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).json({ success: false, message: "Missing credentials" });
+  }
+
+  const { link, text, para, buttonText, bgColor } = req.body;
+  if (!text || !para || !buttonText || !bgColor) {
+    return res.status(400).json({ success: false, message: "Missing credentials" });
+  }
+  try {
+    const imageFile = req.file;
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const aboutPageData = {
+      link,
+      text,
+      para,
+      buttonText,
+      bgColor,
+      image: imageUpload.secure_url,
+    };
+    const aboutPage = await aboutPageModel.findByIdAndUpdate(id, aboutPageData, { new: true });
+    res.status(200).json({ success: true, message: "About page updated successfully", aboutPage });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ success: false, message: "Failed to update about page", error: error.message });
   }
 };
