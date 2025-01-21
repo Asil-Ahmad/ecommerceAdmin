f0f03fe82d931c0911b3c2ade8800d7bec4e31e3
Asil Ahmad <identity101@duck.com>
2025-01-21 15:02:12 +0530

    ✨ feat: Add new image to AboutPageSection and enhance layout with additional content

--- frontend/src/assets/homepage/index.js
+++ frontend/src/assets/homepage/index.js
@@ -1,4 +1,5 @@
 import headphones from "./headphones.jpg";
 import aboutPageSection_img from "./aboutSectionPage.webp"
+import img2 from "./img2.webp"
 
-export { headphones,aboutPageSection_img };
+export { headphones,aboutPageSection_img,img2 };
--- frontend/src/pages/AboutPageSection.jsx
+++ frontend/src/pages/AboutPageSection.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { aboutPageSection_img } from "../assets/homepage";
+import { aboutPageSection_img, img2 } from "../assets/homepage";
 
 const AboutPageSection = () => {
   return (
     <section className='container py-20'>
+      {/* top section */}
       <div className='flex items-start gap-10'>
         <div className='w-1/2'>
           <img src={aboutPageSection_img} alt='' />
         </div>
 
-        <div>
+        <div className='py-10'>
           <h1 className='text-[38px] leading-[48px]'> Lorem ipsum dolor sit amet consectetur.</h1>
           <div className='flex flex-col gap-5 py-5 font-poppins'>
             <div>
@@ -36,15 +37,23 @@ const AboutPageSection = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.
               </p>
             </div>
-            <div>
-              <h2 className='text-[20px] leading-[1.4em] text-red-300 tracking-wider'>
-                Fully controlled system
-              </h2>
-              <p className='text-[20px] leading-[1.4em]'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.
-              </p>
-            </div>
           </div>
+        </div>
+      </div>
+
+      {/* bottom section */}
+      <div className='flex items-center justify-between '>
+        <div className='w-[45.35%]'>
+          <img src={img2} alt='' className='' />
+        </div>
+
+        <div className='w-[51%] flex flex-col gap-6'>
+          <h1 className='text-[38px] leading-[48px]'> Mobile Application to work on the go</h1>
+          <p className='text-[20px] leading-[1.4em]'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil in, cupiditate, ex
+            amet suscipit nam harum nulla tempora obcaecati recusandae porro delectus libero
+            sapiente itaque, laudantium sint. Veniam, distinctio!
+          </p>
         </div>
       </div>
     </section>
