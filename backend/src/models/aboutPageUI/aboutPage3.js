7bf1345a182ca44c743881b2121f43420cbc6587
Asil Ahmad <identity101@duck.com>
2025-01-23 15:38:39 +0530

    ✨ feat: Add aboutPage3 model schema for About Page with text and image fields

---
+++ backend/src/models/aboutPageUI/aboutPage3.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const aboutPage3Schema = new mongoose.Schema({
+  text1: { type: String, required: true },
+  text1Color: { type: String, required: true },
+  text2: { type: String, required: true },
+  text2Color: { type: String, required: true },
+  para1: { type: String, required: true },
+  para1Color: { type: String, required: true },
+  para2: { type: String, required: true },
+  para2Color: { type: String, required: true },
+  image: { type: String, required: true, default: "" },
+  num1: { type: Number, required: true },
+  num2: { type: Number, required: true },
+});
+
+const aboutPage3Model = mongoose.models.aboutPage3 || mongoose.model("aboutPage3", aboutPage3Schema);
+
+export default aboutPage3Model;
